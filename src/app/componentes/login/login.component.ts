c7bd90a7b83f693558459e65690fd21474995ab0
Dighitalsoluciones <hernanglomba@gmail.com>
2022-08-31 16:58:26 -0300

    login components

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -3,12 +3,14 @@ import { Router } from '@angular/router';
 import { LoginUsuario } from 'src/app/model/login-usuario';
 import { AuthService } from 'src/app/service/auth.service';
 import { TokenService } from 'src/app/service/token.service';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css']
+  
   
 })
 export class LoginComponent implements OnInit {
